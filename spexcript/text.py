8fd0d082162f4fa9f51ba6098721e316576fdaba
Koos Zevenhoven <k7hoven@fake.server>
2015-04-15 22:54:47 +0300

    Started spexcript based on old spextex_v2

---
+++ spexcript/text.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 18 15:10:51 2013
+
+Tools for creating plain text spextex output.
+
+@author: Koos Zevenhoven
+"""
+import textwrap
+
+line_end = "\r\n"
+par_indent = 3
+char_descr_indent = 3
+
+
+def wrapped_lines(paragraphs, width = 79): 
+    first = True
+    for par in paragraphs:
+        if not first:
+            par = " " * par_indent + par
+    
+        wrapped_lines = textwrap.wrap(par,width=width)
+    
+        for l in wrapped_lines:
+            yield l
+        first = False
